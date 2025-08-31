345a9796ebc1ec24389d52f43fbc250f03be5cab
RR <rithikbhagat04@gmail.com>
2025-08-31 17:43:42 +0530

    Bug Fixes

--- src/main/java/com/demoqa/factory/DriverFactory.java
+++ src/main/java/com/demoqa/factory/DriverFactory.java
@@ -2,20 +2,26 @@ package com.demoqa.factory;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterSuite;
 
 
 public class DriverFactory {
 
     public static WebDriver driver;
 
-    public void setup(){
+    public WebDriver  setup(){
+         ChromeOptions options = new ChromeOptions();
+         options.addArguments("--headless=new")  ;
          driver = new ChromeDriver();
-         driver.get("https://demoqa.com/");
+        //  driver.get("https://demoqa.com/");
          driver.manage().window().maximize();
-         String title = driver.getTitle();
-         System.out.println(title);
+        //  String title = driver.getTitle();
+        //  System.out.println(title);
+        return driver;
     }
     
+    @AfterSuite
     public void teardown(){
         driver.quit();
     }
--- src/test/java/com/demoqa/AppTest.java
+++ src/test/java/com/demoqa/AppTest.java
@@ -1,9 +1,9 @@
 package com.demoqa;
 
-import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 import com.demoqa.factory.DriverFactory;
 
@@ -14,17 +14,18 @@ public class AppTest extends DriverFactory
 {
    WebDriver driver;
 
-     By textBoxButton = By.id("#item-0");
+     By textBoxButton = By.id("item-0");
 
 @BeforeTest  
 public void elementsPage(){
-    setup();
-    this.driver = driver;
+   driver = setup();
+    
 }
 
 @Test
 public void elements(){
-    driver.get("https://demoqa.com/");
+    driver.get("https://demoqa.com/elements");
     driver.findElement(textBoxButton).click();
+    System.out.println("--------Successfully Done ----------");
 }
 }
